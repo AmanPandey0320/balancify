8c41127849e2ca646b77135331a161fe0382e58d
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-04-11 23:33:17 +0530

    added loadMonitor

--- src/main/java/com/kabutar/balancify/workers/LoadMonitor.java
+++ src/main/java/com/kabutar/balancify/workers/LoadMonitor.java
@@ -1,5 +1,98 @@
 package com.kabutar.balancify.workers;
 
-public class LoadMonitor {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.kabutar.balancify.config.Server;
+
+
+public class LoadMonitor {
+	
+	private Map<String,Integer> serverToIndex;
+	private ArrayList<Integer> reqCount;
+	private ArrayList<Server> servers;
+	
+	/**
+	 * @constructor
+	 * @param requestCount
+	 */
+	public LoadMonitor(ArrayList<Server> servers) {
+		super();
+		this.reqCount = new ArrayList<>(Collections.nCopies(servers.size(), 0));
+		this.serverToIndex = new HashMap<>();
+		this.servers = servers;
+		this.mapServers();
+	}
+	
+	/**
+	 * 
+	 */
+	private void mapServers() {
+		int index = 0;
+		for(Server server:this.servers) {
+			this.serverToIndex.put(server.getId(), index);
+			index++;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 */
+	public void increase(String id) {
+		int currIndex = this.serverToIndex.get(id);
+		int cnt = this.reqCount.get(currIndex) + 1;
+		Server server = this.servers.get(currIndex);
+		
+		this.reqCount.remove(currIndex);
+		this.servers.remove(currIndex);
+		
+		int newIndex = Collections.binarySearch(this.reqCount, cnt);
+		
+		if(newIndex < 0) {
+			newIndex = -(newIndex+1);
+		}
+		
+		this.reqCount.add(newIndex, cnt);
+		this.servers.add(newIndex, server);
+		this.serverToIndex.put(id, newIndex);
+		
+		return;
+		
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 */
+	public void decrease(String id) {
+		int currIndex = this.serverToIndex.get(id);
+		int cnt = this.reqCount.get(currIndex) - 1;
+		Server server = this.servers.get(currIndex);
+		
+		this.reqCount.remove(currIndex);
+		this.servers.remove(currIndex);
+		
+		int newIndex = Collections.binarySearch(this.reqCount, cnt);
+		
+		if(newIndex < 0) {
+			newIndex = -(newIndex+1);
+		}
+		
+		this.reqCount.add(newIndex, cnt);
+		this.servers.add(newIndex, server);
+		this.serverToIndex.put(id, newIndex);
+		
+		return;
+	}
+	
+	public Server getServerWithLeastLoad() {
+		return this.servers.get(0);
+	}
+	
+	
+	
+	
 }
