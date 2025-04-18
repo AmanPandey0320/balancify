77be9c9599e55b4917c1591b8d44b503ad4ccb79
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-04-18 23:53:44 +0530

    added WeightedLeastConnectionScheduler

--- src/main/java/com/kabutar/balancify/scheduler/dynamic/WeightedLeastConnectionScheduler.java
+++ src/main/java/com/kabutar/balancify/scheduler/dynamic/WeightedLeastConnectionScheduler.java
@@ -1,5 +1,16 @@
 package com.kabutar.balancify.scheduler.dynamic;
 
-public class WeightedLeastConnectionScheduler {
-// TODO
+import java.util.ArrayList;
+
+import com.kabutar.balancify.config.Server;
+import com.kabutar.balancify.workers.LoadMonitor;
+
+public class WeightedLeastConnectionScheduler extends LeastConnectionScheduler {
+	
+	private double scalingFactor;
+	public WeightedLeastConnectionScheduler(ArrayList<Server> servers, LoadMonitor loadMonitor, double scalingFactor) {
+		super(servers, loadMonitor);
+		
+		this.scalingFactor = scalingFactor;
+	}
 }
